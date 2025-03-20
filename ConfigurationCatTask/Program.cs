2387f63cdd247a97192b2885be383151023d56ef
fathy <fathy@FATHYALI>
2025-03-20 22:12:08 +0200

    add IOptions setting to work correctly and some code for testing

--- ConfigurationCatTask/Program.cs
+++ ConfigurationCatTask/Program.cs
@@ -1,3 +1,5 @@
+using ConfigurationCatTask;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// add the worker service for test 
+builder.Services.AddScoped<Worker>();
+
+builder.Services.AddOptions<ConfigurationCat>()
+    .Bind(builder.Configuration.GetSection("ConfigurationCat"))
+    .ValidateDataAnnotations();
+
+builder.Services.AddHttpClient<Worker>();
 
 var app = builder.Build();
+using var scope = app.Services.CreateScope();
+var serviceProvider = scope.ServiceProvider;
+var worker = serviceProvider.GetRequiredService<Worker>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
