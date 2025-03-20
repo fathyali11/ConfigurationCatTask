901e7b656a05e492d0ceabcd52b77c2b1d8d9717
fathy <fathy@FATHYALI>
2025-03-20 22:11:28 +0200

    add class for configuration

---
+++ ConfigurationCatTask/ConfigurationCat.cs
@@ -0,0 +1,10 @@
+﻿namespace ConfigurationCatTask;
+
+public class ConfigurationCat
+{
+    public string ApiUrl { get; set; }=string.Empty;
+    public int RetryCount { get; set; }
+    public int DelayMilliseconds { get; set; }
+    public int TimeoutSeconds { get; set; }
+}
+
